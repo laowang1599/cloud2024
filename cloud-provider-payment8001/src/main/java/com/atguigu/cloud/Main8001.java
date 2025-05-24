b7ae5b6e454ab1ea56422510f5749b97525cfb10
wxz <wangxiaozhen2003@outlook.com>
2025-05-25 00:22:14 +0800

    更新代码

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/Main8001.java
@@ -0,0 +1,17 @@
+package com.atguigu.cloud;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+/**
+ * @auther zzyy
+ * @create 2023-11-03 17:54
+ */
+@SpringBootApplication
+@MapperScan("com.atguigu.cloud.mapper") //import tk.mybatis.spring.annotation.MapperScan;
+public class Main8001
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Main8001.class,args);
+    }
+}
