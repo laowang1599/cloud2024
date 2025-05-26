55f45a82fb1fc095da6be129825851f8675b0eec
wxz <wangxiaozhen2003@outlook.com>
2025-05-26 23:55:31 +0800

    新增订单支付包

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/cloud/Main80.java
@@ -0,0 +1,19 @@
+package com.atguigu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @auther zzyy
+ * @create 2025-05-26
+ */
+@SpringBootApplication
+public class Main80
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Main80.class,args);
+    }
+}
+
+
