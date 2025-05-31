74239252f407ab5e1abdfcc3761fd80e29c95dd2
wxz <wangxiaozhen2003@outlook.com>
2025-06-01 00:23:16 +0800

    引入consul

--- cloud-provider-payment8001/src/main/java/com/atguigu/cloud/Main8001.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/Main8001.java
@@ -1,6 +1,8 @@
 package com.atguigu.cloud;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 /**
  * @auther zzyy
@@ -8,6 +10,7 @@ import tk.mybatis.spring.annotation.MapperScan;
  */
 @SpringBootApplication
 @MapperScan("com.atguigu.cloud.mapper") //import tk.mybatis.spring.annotation.MapperScan;
+@EnableDiscoveryClient
 public class Main8001
 {
     public static void main(String[] args)
