b79fcdd8e95dfaa233945dcd8594bf2392b186ff
wxz <wangxiaozhen2003@outlook.com>
2025-06-02 00:17:21 +0800

    修改注册中心上的微服务地址并成功调用

--- cloud-consumer-order80/src/main/java/com/atguigu/cloud/Main80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/cloud/Main80.java
@@ -2,12 +2,14 @@ package com.atguigu.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @auther zzyy
  * @create 2025-05-26
  */
 @SpringBootApplication
+@EnableDiscoveryClient //该注解用于向使用consul为注册中心时注册服务
 public class Main80
 {
     public static void main(String[] args)
